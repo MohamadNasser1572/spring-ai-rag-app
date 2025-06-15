2d66027562fa70d08c0ca4a703e7c11b4f704d9e
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-06-15 20:27:18 +0300

    Initial commit

---
+++ src/main/java/com/nasser/springairagapp/SpringAiRagAppApplication.java
@@ -0,0 +1,13 @@
+package com.nasser.springairagapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiRagAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nasser/springairagapp/SpringAiRagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasser.springairagapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
